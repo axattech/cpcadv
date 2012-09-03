8eda10c6d3dd89a7f36549c5ccdd9b8df62e853b
emamul <emamul.k@axat-tech.com>
2012-09-03 13:30:47 +0530

    commit on 3 september

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -67,7 +67,9 @@ class OffersController < ApplicationController
         Offer.find(insert_id).update_attribute(:random_code, random_string)
               
         format.html { redirect_to @offer, notice: 'Offer was successfully created.' }
-         redirect_to :action=>'promoteOffers?qs=myoffer' 
+       #  redirect_to :action=>'promoteOffers?qs=myoffer' 
+         redirect_to :controller => 'offers', :action => 'promoteAndSortOffers',  :qs => 'myoffer'
+       # match 'promoteOffers', to: 'offers#promoteAndSortOffers', as: 'promoteOffers'
          return
         format.json { render json: @offer, status: :created, location: @offer }
       else
