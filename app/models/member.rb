a05b161be34ef3d4b5c9a00f09eb427a70d1d8e1
emamul <emamul.k@axat-tech.com>
2012-09-05 13:26:56 +0530

    commit on sep 5

--- app/models/member.rb
+++ app/models/member.rb
@@ -9,9 +9,9 @@ class Member < ActiveRecord::Base
   
   
   validates_uniqueness_of :username, :email
-  validates_format_of :email
-    :with => /^(+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/i
-
+  validates_format_of :email,
+:with => /^([^\s]+)((?:[-a-z0-9]\.)[a-z]{2,})$/i
+  
   validates_confirmation_of :password
   
   validates_presence_of :username, :email,  :password, :password_confirmation, :gender, :age, :country_id
